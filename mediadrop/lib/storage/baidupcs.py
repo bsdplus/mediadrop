65e61773ff4f2e246c8746b4689d8a54ca075f8b
Alan Cheng <bsdplus@gmail.com>
2014-04-04 17:05:51 +0800

    trying to start a storage of baidu personal Cloud

---
+++ mediadrop/lib/storage/baidupcs.py
@@ -0,0 +1,8 @@
+from mediacore.lib.storage import StorageEngine
+
+class baidupcs(StorageEngine):
+    """
+    Implement all abstract properties and abstract methods here ...
+    """
+
+StorageEngine.register(baidupcs)
